4178c50b98e537c9b002fdc7395f99a642bc2cc1
hayliblink <zoepagny@gmail.com>
2017-07-18 15:58:57 +0200

    exercice 01

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 module.exports = {
     "development": {
-        db: process.env.MONGODB_URI ||  require('./dev').db,
+        db: process.env.MONGODB_URI ||  require('./dev').db,
         secretToken: process.env.SECRET_TOKEN || 'secretToken',
         env: 'development',
         facebookAuth: {
@@ -10,13 +10,13 @@ module.exports = {
         }
     },
     "production": {
-        db: process.env.MONGODB_URI ||  'mongodb://localhost:27017/blog',
+        db: process.env.MONGODB_URI ||  'mongodb://localhost:27017/blog',
         secretToken: process.env.SECRET_TOKEN || 'secretToken',
         env: 'production',
         facebookAuth: {
-            clientID: process.env.FACEBOOK_ID, // your App ID
-            clientSecret: process.env.FACEBOOK_SECRET, // your App Secret
-            callbackURL: process.env.FACEBOOK_CALLBACK
+            clientID: process.env.FACEBOOK_ID || require('./dev').facebookAuth.clientID, // your App ID
+            clientSecret: process.env.FACEBOOK_SECRET || require('./dev').facebookAuth.clientSecret, // your App Secret
+            callbackURL: process.env.FACEBOOK_CALLBACK || require('./dev').facebookAuth.callbackURL
         }
     }
 }
